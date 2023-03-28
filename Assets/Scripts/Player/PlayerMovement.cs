3b3e1163ba272f78987a3afbcfcf9bd8bc418c20
LayaCube <layapeltier19@gmail.com>
2023-03-28 12:06:29 +0200

    Added Jumping. Closed #1

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,15 +8,18 @@ public class PlayerMovement : MonoBehaviour
     //Game Design Variables
     [SerializeField] private float DefaultSpeed;
     [SerializeField] private float DefaultSprintSpeed;
+    [SerializeField] private float DefaultJumpForce;
 
     //Game Programming Variables
-    private Vector2 m_MovementInput = Vector2.zero;
-    private bool    m_isSprinting     = false;
+    private Vector2     m_MovementInput = Vector2.zero;
+    private bool        m_isSprinting   = false;
+    private bool        m_isJumping     = false;
+    [SerializeField]
+    private Rigidbody m_Rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -25,6 +28,21 @@ public class PlayerMovement : MonoBehaviour
         //Movement depending of input
         float currentSpeed = m_isSprinting == true ? DefaultSprintSpeed : DefaultSpeed;
         transform.position += (transform.right * m_MovementInput.x * currentSpeed) * Time.deltaTime;
+
+        //Jump
+        if (m_isJumping && isThereFloor())
+            m_Rigidbody.AddForce(0, DefaultJumpForce, 0);
+    }
+    
+    //Checks if there is floor under the player.
+    private bool isThereFloor()
+    {
+        //TODO: Make it work
+       foreach (var obj in Physics.OverlapSphere(transform.position - new Vector3(0, 1.2f, 0), 0.1f))
+       {
+            return true;
+       }
+        return false;
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -34,5 +52,10 @@ public class PlayerMovement : MonoBehaviour
     public void Sprint(InputAction.CallbackContext context)
     {
         m_isSprinting = context.ReadValueAsButton();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        m_isJumping = context.ReadValueAsButton();
     }
 }
