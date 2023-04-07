e4adc113df18b4c815f2e1fd131e4c763b627961
g.martineaux <g.martineaux@student.isartdigital.com>
2023-04-07 14:43:12 +0200

    Added Color Switching Platforms - Part of #30

--- Assets/Scripts/Object/ColorObject.cs
+++ Assets/Scripts/Object/ColorObject.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class ColorObject : MonoBehaviour
 {
     //Game Design Variables
-    [SerializeField] public int    colorIndex;
+    [SerializeField] public int[]   colorIndex;
+    [SerializeField] public float   timeBetweenColorSwitch;
 
     // Game Programming Variables
     Renderer m_Renderer;
+    private int     m_Index = 0;
+    private float   m_Timer = 0;
+
+    //Called eslewhere variables
+    public int currentColor = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +25,25 @@ public class ColorObject : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (SingletonPlayerColor.instance.GetPlayerColor() == colorIndex)
+        if (SingletonPlayerColor.instance.GetPlayerColor() == colorIndex[m_Index])
             GetComponent<BoxCollider>().isTrigger = true;
         else
             GetComponent<BoxCollider>().isTrigger = false;
 
-        m_Renderer.material.color = SingletonPlayerColor.instance.SelectableColors[colorIndex];
+        if (m_Timer >= timeBetweenColorSwitch)
+            SwitchColor();
+        m_Timer += Time.deltaTime;
+       
+
+        m_Renderer.material.color = SingletonPlayerColor.instance.SelectableColors[colorIndex[m_Index]];
+        currentColor = m_Index;
     }
 
-   
+   private void SwitchColor()
+    {
+        m_Index++;
+        if (m_Index >= colorIndex.Length)
+            m_Index = 0;
+        m_Timer = 0;
+    }
 }
