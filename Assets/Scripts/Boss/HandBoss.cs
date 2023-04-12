5eac5938f85d5601558fae30a5e07d31b8d3a1a5
g.martineaux <g.martineaux@student.isartdigital.com>
2023-04-12 09:27:29 +0200

    Started Boss - Part of #31

--- Assets/Scripts/Boss/HandBoss.cs
+++ Assets/Scripts/Boss/HandBoss.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class HandBoss : MonoBehaviour
 {
+	//UNCOMMENT
+	//private byte left = 0; private byte right = 1;
+
+	[Header("Game Programming Variables")]
+	[SerializeField]		private bool ShowGPVariables = false;
+	[IGP, SerializeField]	private GameObject[] Hands;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (ShowGPVariables) { } //Avoid a warning.
     }
 
     // Update is called once per frame
